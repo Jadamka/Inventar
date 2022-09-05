5f51e82253a310d719457563ff32b7bf3362295b
Jadamka <59932297+Jadamka@users.noreply.github.com>
2022-09-05 13:37:42 +0200

    Add files via upload

---
+++ Inventar/Inventar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inventar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inv = new Inventory(10);
+
+            
+        }
+    }
+}
